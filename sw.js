a2cb2f9184a5d9ed2f3d7d671b716be7ead0167e
MatredAmandio <Matredamandio@hotmail.com>
2026-09-17 17:18:45 -0300

    Atualiza cache para demonstrações visuais

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "treino-matred-v1";
+const CACHE = "treino-matred-v2";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
