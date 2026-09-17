9bcdd2e03cf02d9ec73db07980527addd7cb8953
MatredAmandio <Matredamandio@hotmail.com>
2026-09-17 18:28:36 -0300

    Atualiza cache das miniaturas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "treino-matred-v2";
+const CACHE = "treino-matred-v3";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
