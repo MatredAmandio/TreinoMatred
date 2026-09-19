02b66cb7199ea0151922756a277c4952602002d8
MatredAmandio <Matredamandio@hotmail.com>
2026-09-19 11:32:08 -0300

    Atualiza cache para imagens lado a lado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "treino-matred-v4";
+const CACHE = "treino-matred-v5";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
