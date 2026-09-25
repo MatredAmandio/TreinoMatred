a11f3a1d8b5b4036fa6615057192389f8ee77e12
MatredAmandio <Matredamandio@hotmail.com>
2026-09-24 23:16:03 -0300

    Atualizar cache para aviso sonoro

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "treino-matred-v6";
+const CACHE = "treino-matred-v7";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
